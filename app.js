59f988ff7ac1333cb51d5e298860e49e474fddbd
JoniWanKenobi <joniwankenoby@gmail.com>
2018-02-28 16:18:52 +0100

    express up set up with auth routes working

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const auth = require('./routes/auth');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/readalin', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+app.use(function (req, res, next) {
+  app.locals.user = req.session.currentUser;
+  next();
+});
+
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({error: 'not found'});
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({error: 'unexpected'});
+  }
+});
+
+module.exports = app;
