effa2236230819063ac7bf0ca89742608dac607c
JoniWanKenobi <joniwankenoby@gmail.com>
2018-03-01 23:27:43 +0100

    pdf.js added in the backend, it extracts and console log the text of any pdf uploaded

--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-
+const PDFJS = require('pdfjs-dist');
 const Book = require('../models/book');
 
 
 // Route to upload from project base path
 const upload = multer({ dest: './public/uploads/' });
 
+PDFJS.disableWorker = true;
+
 router.post('/', upload.single('uploadedFile'), function(req, res){
-  console.log('req.body: ', req.body);
 
   const newBook = new Book({
     path: `/uploads/${req.file.filename}`,
     originalName: req.file.originalname
   });
-    newBook.save((err) => {
-      console.log(err);
-  });
+
+  function gettext(pdfUrl){
+    var pdf = PDFJS.getDocument(pdfUrl);
+    return pdf.then(function(pdf) { // get all pages text
+         var maxPages = pdf.pdfInfo.numPages;
+         var countPromises = []; // collecting all page promises
+         for (var j = 1; j <= maxPages; j++) {
+            var page = pdf.getPage(j);
+    
+            var txt = "";
+            countPromises.push(page.then(function(page) { // add page promise
+                var textContent = page.getTextContent();
+                return textContent.then(function(text){ // return content promise
+                    return text.items.map(function (s) { return s.str; }).join(''); // value page text 
+    
+                });
+            }));
+         }
+         // Wait for all pages and join text
+         return Promise.all(countPromises).then(function (texts) {
+           
+           return texts.join('');
+         });
+    });
+    }
+
+  newBook.save()
+    .then((book) => {
+      console.log('book added okuuy');
+      return gettext('./public'+book.path);
+    })
+    .then((doc) => { 
+      console.log('pdf loaded');
+      console.log('DOC', doc);
+    })
+    .catch(err => console.log(err));
+
+
+
+
+
 });
 
 
